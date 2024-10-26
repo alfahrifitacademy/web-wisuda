88df31d04387e98ad1b415693393570d052fdecb
alfahrifitacademy <alfahri.fitacademy@gmail.com>
2024-10-26 22:56:17 +0700

    Update Dashboard user

--- dashboard.php
+++ dashboard.php
@@ -8,33 +8,86 @@ if (!isset($_SESSION['user_id'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Dashboard User</title>
+    <!-- ======= Styles ====== -->
     <link rel="stylesheet" href="assets/css/dashboard.css">
 </head>
 
 <body>
+    <!-- =============== Navigation ================ -->
     <div class="container">
-        <!-- Sidebar -->
-        <?php include __DIR__ . '/includes/sidebar.php'; ?>
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="dashboard.php">
+                        <span class="icon">
+                            <img src="assets/img/logo.png" alt="Logo STMIK Bandung">
+                        </span>
+                        <span class="title">STMIK Bandung</span>
+                    </a>
+                </li>
 
-        <div class="main-content">
-            <!-- Header -->
-            <?php include __DIR__ . '/includes/header.php'; ?>
+                <li>
+                    <a href="dashboard.php">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
 
-            <div class="content-area">
-                <h2>Dashboard</h2>
-                <p>Selamat datang di Dashboard!</p>
+                <li>
+                    <a href="/daftar_wisuda.php">
+                        <span class="icon">
+                            <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                        <span class="title">Daftar Wisuda</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/kartu_undangan.php">
+                        <span class="icon">
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                        </span>
+                        <span class="title">Kartu Undangan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="logout.php">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span class="title">Sign Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- ========================= Main ==================== -->
+        <div class="main">
+            <div class="topbar">
+                <div class="toggle">
+                    <ion-icon name="menu-outline"></ion-icon>
+                </div>
+                <div class="user">
+                    <img src="assets/img/customer01.png" alt="pp">
+                </div>
             </div>
+            <div class="welcome-section">
+                <h1>SELAMAT DATANG DI <br> WEBSITE PENDAFTARAN WISUDA</h1>
+                <a href="/daftar_wisuda.php" class="btn-daftar">DAFTAR</a>
+            </div>
+            
         </div>
     </div>
 
-    <script src="../assets/js/dashboard.js"></script>
+    <!-- =========== Scripts =========  -->
+    <script src="assets/js/common.min.js"></script>
+    <script src="assets/js/dashboard.js"></script>
+    <!-- ====== ionicons ======= -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
